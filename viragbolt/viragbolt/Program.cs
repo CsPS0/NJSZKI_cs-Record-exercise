da8cbe3fcd698cbe8f5ea64e7289bdaba0aa4049
Solti Csongor Péter <solti.csongor.peter@gmail.com>
2025-02-13 22:54:12 +0100

    Release

---
+++ viragbolt/viragbolt/Program.cs
@@ -0,0 +1,65 @@
+﻿#region 1.fel
+using viragbolt;
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("1. feladat");
+Console.ResetColor();
+
+List<Virag> viragok = new();
+string[] file = File.ReadAllLines("viragok.txt");
+
+foreach (var line in file)
+{
+    string[] datas = line.Split(';');
+    viragok.Add(new Virag(datas[0], int.Parse(datas[1])));
+}
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("A fájl beolvasása sikeres!");
+Console.ResetColor();
+#endregion
+
+#region 2.fel
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("2. feladat");
+Console.ResetColor();
+foreach (var virag in viragok)
+{
+    Console.WriteLine(virag.Kiiras1);
+}
+#endregion
+
+#region 3.fel
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("3. feladat");
+Console.ResetColor();
+Console.WriteLine($"Átlagosan {Virag.AtlagAr(viragok):F2} Ft-ba kerül egy szál virág.");
+#endregion
+
+#region 4.fel
+var legdragabb = Virag.Legdragabb(viragok);
+
+if (legdragabb != null)
+{
+    Console.WriteLine($"A legdrágább virág: {legdragabb.Name} ({legdragabb.Price} Ft)");
+}
+else
+{
+    Console.WriteLine("Nincs elérhető virág az üzletben.");
+}
+#endregion
+
+#region 5.fel
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("5. feladat");
+Console.ResetColor();
+Console.WriteLine($"Legalább 1000 Ft-os virágok száma: {Virag.LegalabbEzerForintos(viragok)}");
+#endregion
+
+#region 6.fel
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("6. feladat");
+Console.ResetColor();
+Console.Write("Add meg egy virág nevét: ");
+string keresett = Console.ReadLine();
+Console.WriteLine(Virag.KeresVirag(viragok, keresett));
+#endregion
---
+++ viragbolt/viragbolt/virag.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viragbolt
+{
+    public record Virag(string Name, int Price)
+    {
+        public string Kiiras1 => $"{Name} ({Price} Ft)";
+
+        public static string AtlagAr(List<Virag> viragok)
+        {
+            double atlag = viragok.Sum(v => v.Price) / (double)viragok.Count;
+            return $"Átlagosan {atlag:F2} Ft-ba kerül egy szál virág.";
+        }
+
+        public static Virag? Legdragabb(List<Virag> viragok)
+        {
+            if (viragok.Count == 0)
+            {
+                return null;
+            }
+
+            Virag legdragabb = viragok[0];
+
+            foreach (var virag in viragok)
+            {
+                if (virag.Price > legdragabb.Price)
+                {
+                    legdragabb = virag;
+                }
+            }
+
+            return legdragabb;
+        }
+
+        public static string LegalabbEzerForintos(List<Virag> viragok)
+        {
+            int db = viragok.Count(v => v.Price >= 1000);
+            return $"Legalább 1000 Ft-os virágok száma: {db}";
+        }
+
+        public static string KeresVirag(List<Virag> viragok, string nev)
+        {
+            foreach (var v in viragok)
+            {
+                if (v.Name.Equals(nev, StringComparison.OrdinalIgnoreCase))
+                {
+                    return $"{v.Name} ({v.Price} Ft)";
+                }
+            }
+            return "Ez a virág nincs az üzletben.";
+        }
+    }
+}
